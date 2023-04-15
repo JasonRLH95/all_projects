a432958345e1c5662b22eb645127133cb144f840
JasonRLH95 <jason95.rm@gmail.com>
2023-04-16 00:23:07 +0300

    last update - version 1.0

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -6,7 +6,7 @@ export default function Link(props) {
         <div>
             <p className='linksP'>{props.name}</p>
             <div className='linksDiv'>
-                <a href={props.link}><img className='linkImg' src={props.image} alt='img'></img></a>
+                <a target='_blank' href={props.link}><img className='linkImg' src={props.image} alt='img'></img></a>
             </div>
             <p className='linksDesc'>{props.desc}</p>
         </div>
