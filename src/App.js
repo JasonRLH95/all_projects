e8240d55bab03132555c94ba56ca3adf717d846d
JasonRLH95 <jason95.rm@gmail.com>
2023-05-09 03:28:46 +0300

    add bankApp project

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Link from './components/Link';
 import image1 from './images/calc_image.png';
 import image2 from './images/cards_image.png';
 import image3 from './images/sudoku_image.png';
+import image4 from './images/bankApp_image.png';
 
 function App() {
   const links = [
@@ -10,7 +11,7 @@ function App() {
       name:'event calculator',
       link:"https://jasonrlh95.github.io/event_calc_app/",
       image:image1,
-      desc:'A calculator web/app responsively that calculate for events hall workers how mant baskets of wine cups/egg cups/plates they need relative to the amount of tables/clients per event'
+      desc:'A calculator web/app responsively that calculate for events hall workers how many baskets of wine cups/egg cups/plates they need relative to the amount of tables/clients per event'
     },
     {
       name:'cards game',
@@ -23,6 +24,12 @@ function App() {
       link:"https://jasonrlh95.github.io/sudoku/",
       image:image3,
       desc:'A responsive for web/mobile sudoku game, first there is a login page, then dufficulty level choosing and then the famous familiar game'
+    },
+    {
+      name:'bank app',
+      link:'https://jasonrlh95.github.io/bankApp/',
+      image:image4,
+      desc:'bank management app - mobile styled alike'
     }
   ]
   return (
