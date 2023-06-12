b46d5287815f3268cdd96aa544e4fab3c797200e
JasonRLH95 <jason95.rm@gmail.com>
2023-06-12 04:39:20 +0300

    update projects

--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
 import './App.css';
 import Link from './components/Link';
-import image1 from './images/calc_image.png';
-import image2 from './images/cards_image.png';
-import image3 from './images/sudoku_image.png';
-import image4 from './images/bankApp_image.png';
+import image1 from './images/garage_image.png';
+import image2 from './images/sudoku_image.png';
+import image3 from './images/bankApp_image.png';
+import image4 from './images/cards_image.png';
+// import image5 from './images/calc_image.png';
 
 function App() {
   const links = [
     {
-      name:'event calculator',
-      link:"https://jasonrlh95.github.io/event_calc_app/",
+      name:'Garage manager',
+      link:"https://jasonrlh95.github.io/garage_app/",
       image:image1,
-      desc:'A calculator web/app responsively that calculate for events hall workers how many baskets of wine cups/egg cups/plates they need relative to the amount of tables/clients per event'
+      desc:"Responsive mobile/web site for managing a car issues and repairs history"
     },
     {
-      name:'cards game',
-      link:"https://jasonrlh95.github.io/cards_game_app/",
+      name:'Sudoku game',
+      link:"https://jasonrlh95.github.io/sudoku/",
       image:image2,
+      desc:'Responsive web/mobile sudoku game, first there is a login page, then dufficulty level choosing and then the famous familiar game'
+    },
+    {
+      name:'Bank expenses app',
+      link:'https://jasonrlh95.github.io/bankApp/',
+      image:image3,
+      desc:'Expenses management responsive mobile/web site'
+    },
+    {
+      name:'Cards war game',
+      link:"https://jasonrlh95.github.io/cards_game_app/",
+      image:image4,
       desc:'War card game against the computer responsive for web/mobile - let the best wins!'
     },
-    {
-      name:'sudoku game',
-      link:"https://jasonrlh95.github.io/sudoku/",
-      image:image3,
-      desc:'A responsive for web/mobile sudoku game, first there is a login page, then dufficulty level choosing and then the famous familiar game'
-    },
-    {
-      name:'bank app',
-      link:'https://jasonrlh95.github.io/bankApp/',
-      image:image4,
-      desc:'bank management app - mobile styled alike'
-    }
+    // {
+    //   name:'event calculator',
+    //   link:"https://jasonrlh95.github.io/event_calc_app/",
+    //   image:image5,
+    //   desc:'A calculator web/app responsively that calculate for events hall workers how many baskets of wine cups/egg cups/plates they need relative to the amount of tables/clients per event'
+    // },
   ]
   return (
     <div className="App">
